9151ba0d55ce2c387d68b8c118c2819b1b30ac57
Younes LOUREIRO <ylour@lmdsio.com>
2026-03-13 09:17:04 +0100

    manque pour aller plus loin

--- src/main/java/com/lmdsio/javajfx_chap1/LoginController.java
+++ src/main/java/com/lmdsio/javajfx_chap1/LoginController.java
@@ -4,9 +4,13 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
+import java.util.List;
+
 public class LoginController {
     @FXML
     private Label loginText;
+    @FXML
+    private Button logoutButton;
     @FXML
     private TextField loginField;
     @FXML
@@ -16,9 +20,13 @@ public class LoginController {
     @FXML
     private int errorCount = 0;
     @FXML
+    private ListView<String> mylistView;
+
+    @FXML
     protected void onLoginButtonClick() {
         String login = loginField.getText();
         String password = passwordField.getText();
+        logoutButton.setDisable(true);
         if (!password.equals("mdp")) {
             errorCount++;
             loginText.setText("Identifiants incorrects " + errorCount +" /3");
@@ -39,6 +47,9 @@ public class LoginController {
             loginText.setStyle("-fx-text-fill: green");
             loginText.setText("Hello " + login);
             errorCount = 0;
+            loginButton.setDisable(true );
+            logoutButton.setDisable(false );
+
         }
 
     }
@@ -48,6 +59,9 @@ public class LoginController {
         passwordField.clear();
         loginText.setText("Veuillez saisir vos identifiants");
         loginText.setStyle("-fx-text-fill: black");
+        logoutButton.setDisable(true);
+        loginButton.setDisable(false);
 
     }
+
 }
